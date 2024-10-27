b908326ed7d95a04b095f1db153bc87b3a1e14ee
yucheng.yu <yucheng.yu@mile.cloud>
2024-10-28 02:10:17 +0800

    Serve local model

--- llm/llama_cpp_python_serve.py
+++ llm/llama_cpp_python_serve.py
@@ -1,15 +1,25 @@
 from llama_cpp import Llama
 
-llm = Llama.from_pretrained(
-	repo_id="bartowski/Meta-Llama-3.1-8B-Instruct-GGUF",
-	filename="Meta-Llama-3.1-8B-Instruct-IQ2_M.gguf",
+# conda create -n llama-cpp-python python=3.9
+# pip install llama-cpp-python huggingface-hub
+
+
+# llm = Llama.from_pretrained(
+# 	repo_id="bartowski/Meta-Llama-3.1-8B-Instruct-GGUF",
+# 	filename="./prebuilt_model/Meta-Llama-3.1-8B-Instruct-IQ2_M.gguf",
+# )
+
+llm = Llama(
+    model_path="./prebuilt_model/Meta-Llama-3.1-8B-Instruct-IQ2_M.gguf"
 )
 
-llm.create_chat_completion(
-	messages = [
-		{
-			"role": "user",
-			"content": "What is the capital of France?"
-		}
-	]
+result = llm.create_chat_completion(
+    messages=[
+        {
+            "role": "user",
+            "content": "What is the capital of France?"
+        }
+    ]
 )
+
+print(result)
